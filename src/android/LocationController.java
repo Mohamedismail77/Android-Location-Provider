364429395f64a02b2b4fe15c60f5d8b9bb39f26c
m-ismail <mohamedismail77@yahoo.com>
2018-05-26 04:20:00 +0200

    create getLastKnownLocation interface

--- src/android/LocationController.java
+++ src/android/LocationController.java
@@ -1,7 +1,230 @@
 package com.cordova.plugin.location.provider;
 
 
-public class LocationController {
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class LocationController implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private Context mContext;
+    private Activity mActivity;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private GoogleApiClient mGoogleApiClient;
+    private Task<LocationSettingsResponse> task;
+    private final int REQUEST_CHECK_SETTINGS = 105;
+    private final int LOCATION_SETTING_REQUEST = 106;
+    private Location mCurrentLocation;
+    private boolean mRequestingLocationUpdates = false;
+    private LocationCallback mLocationCallback;
+    private final String REQUEST_LOCATION_UPDATES_KEY = "location_update";
+    private final String CURRENT_LOCATION_KEY = "current_location";
+    private final int LOCATION_UPDATE_INTERVAL = 1000;
+    private final int LOCATION_FASTEST_UPDATE_INTERVAL = 400;
+    //private AddressResultReceiver mResultReceiver;
+    //private String mAddressOutput;
+
+    public LocationController(Activity activity){
+        mActivity = activity;
+        mContext = activity.getApplicationContext();
+        //Create google api client
+        mGoogleApiClient = new GoogleApiClient.Builder(mContext)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        // Connect google api client
+        mGoogleApiClient.connect();
+
+    }
+
+    //Initiate FusedLocation and location request
+    private void setmFusedLocationClient(int updateInterval, int fastestUpdateInterval){
+        if (checkForLocationPermissions()) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mContext);
+            setLocationRequest(LocationRequest.PRIORITY_HIGH_ACCURACY,
+                    updateInterval,
+                    fastestUpdateInterval);
+        }
+    }
+
+    //Check for android sdk version
+    //if sdk >= 23 ask for permission
+    private boolean checkForLocationPermissions(){
+
+        if(Build.VERSION.SDK_INT >= 23){
+            //Check for location permissions which declared in manifest
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }else{
+                return true;
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    /* Setting the parameter to set the location request
+     * @param: priority => the priority of the request, which gives the Google Play services location services,
+     *         a strong hint about which location sources to use.Check documentation for supported values;
+     *
+     * @param: updateInterval => the rate in milliseconds at which your app prefers to receive location updates.
+     *        **Note that the location updates may be faster than this rate if another app is receiving updates,
+     *        at a faster rate, or slower than this rate, or there may be no updates at all
+     *        (if the device has no connectivity, for example);
+     *
+     * @param: fastestUpdateInterval =>  the fastest rate in milliseconds at which your app can handle location updates;
+     *
+     */
+
+    public void setLocationRequest(final int priority, final int updateInterval, final int fastestUpdateInterval){
+        //Get current location settings
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        //Next check whether the current location settings are satisfied
+        SettingsClient client = LocationServices.getSettingsClient(mActivity);
+        task = client.checkLocationSettings(builder.build());
+
+        //Check setting if valid or not
+        task.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                if (mRequestingLocationUpdates) {
+                    //startLocationUpdates();
+                } else {
+                    getLastKnownLocation();
+                }
+            }
+        });
+
+        //Check for that all settings are satisfied
+        task.addOnSuccessListener(mActivity, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                //Initiate Location request
+                mLocationRequest = new LocationRequest();
+                //Set it's accuracy to high
+                mLocationRequest.setPriority(priority);
+                //Set update interval to be every 1s
+                mLocationRequest.setInterval(updateInterval);
+                //Set the upper limit to update rate
+                mLocationRequest.setFastestInterval(fastestUpdateInterval);
+
+
+            }
+        });
+
+        task.addOnFailureListener(mActivity, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(mActivity,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+
+
+
+    }
+
+    /*
+     * Get last known location from fused location this
+     * checking if the location is null and resolve this problem;
+     *
+     */
+    @SuppressLint("MissingPermission")
+    private void getLastKnownLocation(){
+
+        mFusedLocationClient.flushLocations();
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(mActivity, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    mCurrentLocation = location;
+                } else {
+                    //check location setting
+                    //Get current location settings
+                    //Get current location settings
+                    setmFusedLocationClient(LOCATION_UPDATE_INTERVAL, LOCATION_FASTEST_UPDATE_INTERVAL);
+                    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                            .addLocationRequest(mLocationRequest);
+                    //Next check whether the current location settings are satisfied
+                    SettingsClient client = LocationServices.getSettingsClient(mActivity);
+                    task = client.checkLocationSettings(builder.build());
+                    Toast.makeText(mContext, "Location not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // Check for location permissions is granted or not
+        // Initiate fusedLocationClient
+        setmFusedLocationClient(LOCATION_UPDATE_INTERVAL, LOCATION_FASTEST_UPDATE_INTERVAL);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        try {
+            if (connectionResult.getResolution() != null) {
+                connectionResult.getResolution().send();
+            }
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Location getmCurrentLocation(){
+        return mCurrentLocation;
+    }
+
+
 }
--- src/android/LocationProvider.java
+++ src/android/LocationProvider.java
@@ -1,73 +1,140 @@
 package com.cordova.plugin.location.provider;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.provider.Settings;
+import android.widget.Toast;
+
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.location.Location;
-import android.widget.Toast;
-import android.content.Context;
+import static android.app.Activity.RESULT_CANCELED;
 
 
 //Native interface class as defined in plugin.xml
 public class LocationProvider extends CordovaPlugin {
 
+    private final int REQUEST_CHECK_SETTINGS = 105;
+    private final int LOCATION_SETTING_REQUEST = 106;
+    private LocationController mLocationController;
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
+        mLocationController = new LocationController(cordova.getActivity());
     }
 
     /*
-    * Communicate with  js interface and accept methods and parameters;
-    * @param action: called method by name as string from js interface;
-    * @param 
-    */
+     * Communicate with  js interface and accept methods and parameters;
+     * @param action: called method by name as string from js interface;
+     * @param args: json array contains argument passed to action;
+     * @param callbackContext: callback function success and error;
+     * @return: boolean as true if action found and called and false else;
+     */
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        switch (action){
+            case "getCurrentLocation":
+                getLastKnownLocation(callbackContext);
+                return true;
+            case "getLocationUpdate":
+                getLocationUpdate(args.getInt(0),args.getInt(1),callbackContext);
+                return true;
+        }
         return super.execute(action, args, callbackContext);
     }
 
     /*
-    * Interface for js to get current location using google play service;
-    * @return: success callback contain the current location in form of longitude and latitude;
-    */
-    private void getLastKnownLocation(){
-
+     * Interface for js to get current location using google play service;
+     * @return: success callback contain the current location in form of longitude and latitude;
+     */
+    private void getLastKnownLocation(CallbackContext callbackContext) throws JSONException {
+        Location location = mLocationController.getmCurrentLocation();
+        //Return location latitude and longitude as json object
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("latitude",location.getLatitude());
+        jsonObject.put("longitude",location.getLongitude());
+        callbackContext.success( jsonObject);
     }
 
     /*
-    * Interface for js to get location update using google play service;
-    * @param: interval => an integer refer to interval of getting location update;
-    * @param: fastestUpdateInterval =>  the fastest rate in milliseconds at which your app can handle location updates;
-    * @return: plugin result not a success callback to remain the listener;
-    */
-    private void getLocationUpdate(int updateInterval,int fastestUpdateInterval){
-
+     * Interface for js to get location update using google play service;
+     * @param: interval => an integer refer to interval of getting location update;
+     * @param: fastestUpdateInterval =>  the fastest rate in milliseconds at which your app can handle location updates;
+     * @return: plugin result not a success callback to remain the listener;
+     */
+    private void getLocationUpdate(int updateInterval,int fastestUpdateInterval,CallbackContext callbackContext){
+        
+        // set new location request paramter
+        
+        //set location callback
+        
+        //set plugin result
+        
     }
-    
+
     /*
-    * Interface for js to get location Address using Geocoder service;
-    * @param: latitude => a double refer to latitude of requesting address;
-    * @param: longitude =>  a double refer to longitude of requesting address;
-    * @return: plugin  success callback if address is found || and error callback in case of null address;
-    */
+     * Interface for js to get location Address using Geocoder service;
+     * @param: latitude => a double refer to latitude of requesting address;
+     * @param: longitude =>  a double refer to longitude of requesting address;
+     * @return: plugin  success callback if address is found || and error callback in case of null address;
+     */
 
     private void getLocationAddress(double latitude, double longitude){
 
     }
 
     /*
-    * Interface for js to stop google play service;
-    * must be called at onPause or onDestroy;
-    */
+     * Interface for js to stop google play service;
+     * must be called at onPause or onDestroy;
+     */
     private void stop(){
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CHECK_SETTINGS){
+            //Check if the user give answer ok or cancel
+            if(resultCode == RESULT_CANCELED){
+                //Alert user to open location settings
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(cordova.getContext());
+                alertDialog.setTitle("Location is disabled");
+                alertDialog.setMessage("Please enable location to get location updates");
+                alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Open location Setting
+                        Intent viewIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+
+                        cordova.startActivityForResult(LocationProvider.this,viewIntent,LOCATION_SETTING_REQUEST);
+
+                    }
+                });
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialog.create().show();
+
+            }
+
+        }
+
+    }
+
+
 
 }
 
